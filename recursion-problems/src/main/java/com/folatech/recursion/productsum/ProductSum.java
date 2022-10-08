09faba515e18aeb9611aa24b707761cf06494e38
folaulau <folaukaveinga@gmail.com>
2022-10-08 12:29:59 -0600

    Update ProductSum.java

--- recursion-problems/src/main/java/com/folatech/recursion/productsum/ProductSum.java
+++ recursion-problems/src/main/java/com/folatech/recursion/productsum/ProductSum.java
@@ -1,10 +1,32 @@
 package com.folatech.recursion.productsum;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ * Problem:<br>
+ * 
+ * Write a function that takes in a "special" array and returns its product sum.
+ * <br>
+ * A "special" array is a non-empty array that contains either integers or other
+ * "special" arrays. The product sum of a "special" array is the sum of its
+ * elements, where "special" arrays inside it are summed themselves and then
+ * multiplied by their level of depth. <br>
+ * The depth of a "special" array is how far nested it is. For instance, the
+ * depth of [] is 1; the depth of the inner array in [[]] is 2; the depth of the
+ * innermost array in [[[]]] is 3. <br>
+ * 
+ * Therefore, the product sum of [x, y] is x + y; the product sum of [x, [y, z]]
+ * is x + 2 * (y + z); the product sum of [x, [y, [z]]] is x + 2 * (y + 3z).
+ * 
+ * <br>
+ * Sample Input <br>
+ * 
+ * 
+ * array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]] <br>
+ * Sample Output
+ * 
+ * calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
+ */
 public class ProductSum {
 
 	public static void main(String[] args) {
@@ -22,8 +44,6 @@ public class ProductSum {
 		int productSum = 0;
 		for (Object obj : objs) {
 			String typeName = obj.getClass().getTypeName();
-
-			System.out.println("typeName: " + typeName);
 
 			if (typeName.startsWith("java.util.ImmutableCollections$List")
 					|| typeName.startsWith("java.util.ArrayList")) {
